92e9d2fbbb627fac75b7f5cad59ba786dc1748f9
Sylas Souza <sylas@Sylass-Mac-mini.local>
2026-09-22 18:30:29 -0500

    Revert "feat: slice and integrate 108 rune piece sprites with dynamic Star King morphs and theme palettes"
    This reverts commit 76e44bfb47fa45a4a38758e7bf4f0220ed79f527.

--- scripts/Piece.gd
+++ scripts/Piece.gd
@@ -64,16 +64,10 @@ func apply_theme(p_theme: Dictionary) -> void:
 
 func _load_current_texture() -> void:
 	var tex_path = ""
-	if is_king:
-		if player == Player.RED:
-			tex_path = theme_data.get("red_king_texture", "res://textures/runes/crimson_red_star.png")
-		else:
-			tex_path = theme_data.get("black_king_texture", "res://textures/runes/sand_light_star.png")
+	if player == Player.RED:
+		tex_path = theme_data.get("red_texture", "res://textures/piece_dark.png")
 	else:
-		if player == Player.RED:
-			tex_path = theme_data.get("red_texture", "res://textures/runes/crimson_red_cross.png")
-		else:
-			tex_path = theme_data.get("black_texture", "res://textures/runes/sand_light_circle.png")
+		tex_path = theme_data.get("black_texture", "res://textures/piece_light.png")
 
 	if ResourceLoader.exists(tex_path):
 		piece_texture = load(tex_path)
@@ -203,7 +197,6 @@ func move_to(new_grid_pos: Vector2i, animate: bool = true) -> void:
 
 func promote_to_king() -> void:
 	is_king = true
-	_load_current_texture()
 	queue_redraw()
 	var tween = create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "scale", Vector2(1.25, 1.25), 0.15)
